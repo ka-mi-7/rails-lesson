1a3850a4567e61ec3821353048bf0541c965d2f5
masayuki <kik557473@gmail.com>
2020-08-30 16:02:51 +0000

    create routers.jp

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,8 @@
 class PostsController < ApplicationController
   def index
-    # viewで定義していた配列postsを@postsという変数名で定義してください
-    @posts = [
-      "今日からProgateでRailsの勉強するよー！",
-      "投稿一覧ページ作成中！"
-    ]
+    # @postsに代入されている配列をPost.allに書き換えてください
+    @posts = 
+      Post.all
+   
   end
 end
